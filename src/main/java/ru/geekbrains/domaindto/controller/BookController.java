b38eb1ec6f334a5cc4b8c487f3bd95f6830d2b9e
Maxim Kalashnikov <MIKalashnikov.SBT@sberbank.ru>
2017-11-20 01:06:08 +0300

    initNewApp

--- src/main/java/ru/geekbrains/domaindto/controller/BookController.java
+++ src/main/java/ru/geekbrains/domaindto/controller/BookController.java
@@ -1,18 +1,12 @@
 package ru.geekbrains.domaindto.controller;
 
 import org.jooq.example.db.h2.tables.pojos.Book;
-import org.jooq.example.db.h2.tables.records.BookRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.geekbrains.domaindto.model.Test;
 import ru.geekbrains.domaindto.service.BookService;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
 @RequestMapping("/books")
